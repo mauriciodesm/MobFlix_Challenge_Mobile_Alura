2005f00dd950a47f8c32de4e45e01f5cb6076e37
Mauricio Maciel <mauriciomaciel@id.uff.br>
2022-10-19 16:21:02 -0300

    Storyboard deleted + initial configuration

---
+++ MobFlix/MobFlix/Debug/DebugViewController.swift
@@ -0,0 +1,43 @@
+
+import UIKit
+
+/**
+
+ Use this ViewController to debug your View components.
+
+ 1) Change `myView` type to your UIView subclass
+ 2) Set `myViewHeight` according to your View
+ 3) Set an instance of DebugViewController as window's rootViewController in `SceneDelegate.swift` file
+
+ */
+
+class DebugViewController: UIViewController {
+/*
+    private let myView: HomeView = {
+        let view = ConfirmationView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private let myViewHeight: CGFloat = 100
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        view.backgroundColor = .white
+        view.addSubview(myView)
+
+        NSLayoutConstraint.activate([
+            myView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            myView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            myView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+//          myView.heightAnchor.constraint(equalToConstant: myViewHeight)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+ 
+ */
+}
+
---
+++ MobFlix/MobFlix/Screens/Home/HomeController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
+
